be18acc6a9de314b3698d7857e554cc7a08266ce
Tolik9924 <76564089+Tolik9924@users.noreply.github.com>
2022-08-29 12:06:09 +0300

    Fix Footer

--- ui/src/components/common/Footer/styled.js
+++ ui/src/components/common/Footer/styled.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const Container = styled.footer`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -15,21 +15,21 @@ export const Container = styled.div`
 
 export const Logo = styled.p`
   color: #002f32;
-  padding: 2% 0px 2% 0px;
+  padding: 25px 0px 25px 0px;
   font-size: 40px;
   font-weight: 600;
 `;
 
 export const AddInfo = styled.p`
   color: #002f32;
-  padding-bottom: 1%;
+  padding-bottom: 20px;
   width: 30%;
   font-size: 14px;
 `;
 
 export const Copy = styled.p`
   color: #002f32;
-  padding-bottom: 1%;
+  padding-bottom: 20px;
   font-size: 12px;
   font-weight: 600;
 `;
