dc379421d9a5ead5ea5cff49a1c3c7fa40eb10a6
nadiakohli <andruknadia@gmail.com>
2022-09-01 12:59:20 +0300

    rename file and delete file

--- ui/src/sevices/authServices.js
+++ ui/src/sevices/authServices.js
@@ -1,14 +1,14 @@
 import API from '../API';
-import ServerException from '../exceptions/serverException';
 
-const apiUserServices = {
+const apiUserService = {
   login: async (email, password) => {
     try {
       const user = API.post('/users/login', { email, password });
       return user;
     } catch (e) {
-      throw new ServerException(e.response);
+      console.log(e);
     }
+    return null;
   },
 
   registration: async (email, password) => {
@@ -16,47 +16,10 @@ const apiUserServices = {
       const user = API.post('/users/registration', { email, password });
       return user;
     } catch (e) {
-      throw new ServerException(e.response);
+      console.log(e);
     }
-  },
-  logout: async () => {
-    try {
-      const user = API.post('/users/logout');
-      return user;
-    } catch (e) {
-      throw new ServerException(e.response);
-    }
-  },
-  modify: async (id, {
-    email,
-    password,
-    fullName,
-    address,
-    phoneNumber,
-  }) => {
-    try {
-      const body = {
-        email,
-        password,
-        fullName,
-        address,
-        phoneNumber,
-      };
-      const userModify = await API.patch(`/users${id}`, body);
-
-      return userModify;
-    } catch (e) {
-      throw new ServerException(e.response, 'errors');
-    }
-  },
-  deleteUser: async (id) => {
-    try {
-      const deleteUser = await API.delete(`/users${id}`);
-      return deleteUser;
-    } catch (e) {
-      throw new ServerException(e.response, 'errors');
-    }
+    return null;
   },
 };
 
-export default apiUserServices;
+export default apiUserService;
