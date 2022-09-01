ed3b4cb10e263f720c0b203ef7c6bc3214282a26
Mr-Oleksandr <skovalov777@gmail.com>
2022-09-01 13:34:06 +0300

    create-category

---
+++ api/src/models/Category.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const categorySchema = new mongoose.Schema({
+  name: {
+    type:String, required: true, trim: true,
+  },
+  slug:{
+    type:String, required: true, unique: true,
+  },
+  parentId: {
+    type:String,
+  },
+}, {
+  timestamps:true,
+});
+
+module.exports = mongoose.model('Category', categorySchema);
