394272657b6aa44d419783f0fe94cc5c251d6feb
MaxSaveskul <maxsaveskul@gmail.com>
2022-09-06 17:24:52 +0300

    theme object is replaced

--- ui/src/index.js
+++ ui/src/index.js
@@ -2,12 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'App';
 import { ThemeProvider } from 'styled-components';
+import theme from 'helpers/theme';
 import GlobalStyle from './GlobalStyle';
-
-const theme = {
-  mainGreenColor: '#002f34',
-  greyBackground: '#f2f4f5',
-};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
