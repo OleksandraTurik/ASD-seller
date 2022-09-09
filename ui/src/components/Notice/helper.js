ed464ec9497f1c4f056e20a1161c861e6b2bf40c
Tolik9924 <76564089+Tolik9924@users.noreply.github.com>
2022-09-09 12:51:39 +0300

    styled error and warning in registration

--- ui/src/components/Notice/helper.js
+++ ui/src/components/Notice/helper.js
@@ -2,13 +2,16 @@ export const noticeColor = {
   default: {
     primaryColor: '#d9d9d9',
     secondaryColor: '#e8e8e8',
+    height: '40px',
   },
   warning: {
     primaryColor: '#faad14',
     secondaryColor: '#ffe58f',
+    height: '80px',
   },
   error: {
     primaryColor: '#ff4d4f',
     secondaryColor: '#ff7875',
+    height: '40px',
   },
 };
--- ui/src/components/Notice/styled.js
+++ ui/src/components/Notice/styled.js
@@ -1,3 +1,4 @@
+import { type } from '@testing-library/user-event/dist/type';
 import styled from 'styled-components';
 import { noticeColor } from './helper';
 
@@ -6,6 +7,14 @@ export const WrapperNotice = styled.div`
     border: 1px solid ${({ type }) => noticeColor[type].primaryColor};
     background-color: ${({ type }) => noticeColor[type].secondaryColor};
     width: 100%;
-    height: 40px;
+    height: ${({ type }) => noticeColor[type].height};
     border-radius: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 40px;
+`;
+
+export const Span = styled.span`
+  width: 280px;
 `;
