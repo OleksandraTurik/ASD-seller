cac521bed02426451b8b3e060acb89aaeaaf598b
nadiakohli <andruknadia@gmail.com>
2022-09-10 15:41:45 +0300

    add profile, login and logout buttons

--- ui/src/components/common/Header/index.jsx
+++ ui/src/components/common/Header/index.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 // Icons
 import Logo from 'assets/icons/Logo';
 import Like from 'assets/icons/Like';
+import Login from 'assets/icons/Login';
+import Logout from 'assets/icons/Logout';
+import User from 'assets/icons/User';
 
 // Styles
 import {
@@ -16,34 +20,87 @@ import {
   NavLinkHeader,
 } from './styled';
 
-const Header = () => (
-  <Container>
-    <Nav>
-      <NavLinkHeader to="/">
-        <Logo
-          width="250px"
-          fill="#fff"
-        />
-      </NavLinkHeader>
-      <Ul>
-        <Li>
-          <A href="*">
-            <Like
-              width="25px"
-              height="25px"
-              fill="#fff"
-            />
-          </A>
-        </Li>
-        <Li>
-          <NavLinkAdverts to="/add">Додати оголошення</NavLinkAdverts>
-        </Li>
-        <Li>
-          <NavLink to="/login">Login</NavLink>
-        </Li>
-      </Ul>
-    </Nav>
-  </Container>
-);
+const Header = () => {
+  const navigate = useNavigate();
+  const user = JSON.parse(localStorage.getItem('tokens'));
+  const userId = user ? user.userDto.id : null;
+  const test = localStorage.getItem('tokens');
+
+  const logOut = () => {
+    localStorage.clear();
+    navigate('/login', { replace: true });
+  };
+
+  useEffect(() => console.log(test, userId), [test, userId]);
+  return (
+    <Container>
+      <Nav>
+        <NavLinkHeader to="/">
+          <Logo
+            width="250px"
+            fill="#fff"
+          />
+        </NavLinkHeader>
+        <Ul>
+          <Li>
+            <A href="*">
+              <Like
+                width="25px"
+                height="25px"
+                fill="#fff"
+              />
+            </A>
+          </Li>
+          {test
+            ? (
+              <Li>
+                <NavLinkAdverts to="/add">Додати оголошення</NavLinkAdverts>
+              </Li>
+            )
+            : (
+              <Li>
+                <NavLinkAdverts to="/login">Додати оголошення</NavLinkAdverts>
+              </Li>
+            )}
+          <Li>
+            <NavLink to={`/profiles/${userId}/adverts`}>
+              <User
+                width="25px"
+                height="25px"
+                fill="#fff"
+              />
+            </NavLink>
+          </Li>
+          {!test
+            ? (
+              <Li>
+                <NavLink to="/login">
+                  <Login
+                    width="25px"
+                    height="25px"
+                    fill="#fff"
+                  />
+                </NavLink>
+              </Li>
+            )
+            : (
+              <Li>
+                <NavLink
+                  onClick={logOut}
+                  to="/login"
+                >
+                  <Logout
+                    width="25px"
+                    height="25px"
+                    fill="#fff"
+                  />
+                </NavLink>
+              </Li>
+            )}
+        </Ul>
+      </Nav>
+    </Container>
+  );
+};
 
 export default Header;
