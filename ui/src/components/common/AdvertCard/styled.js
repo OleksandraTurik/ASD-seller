f354cc85d102e05dd8788fdd747a860dfbfb25df
nadiakohli <andruknadia@gmail.com>
2022-09-15 12:40:45 +0300

    fix address, add image and change heart in adverts on main page

--- ui/src/components/common/AdvertCard/styled.js
+++ ui/src/components/common/AdvertCard/styled.js
@@ -74,9 +74,8 @@ export const ToFavoriteButton = styled.button`
   bottom: 12px;
   right: 20px;
   cursor: pointer;
-  opacity: 0.4;
 
-  &:hover {
-    opacity: 1;
+  &:hover svg {
+    fill: #002f34;
   }
 `;
