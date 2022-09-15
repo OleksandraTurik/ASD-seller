2bde91b11a81ea9d91d7bfda31e72e670437b6a1
Mr-Oleksandr <skovalov777@gmail.com>
2022-09-16 01:51:36 +0300

    add-activated-message

--- ui/src/pages/NotFound/index.jsx
+++ ui/src/pages/NotFound/index.jsx
@@ -4,21 +4,29 @@ import React from 'react';
 import cat from 'assets/icons/cat.svg';
 
 // Styles
+import PropTypes from 'prop-types';
 import {
   Wrapper,
   Icon,
   GoHomeLink,
   Container,
 } from './styled';
+import NotFoundAdverts from '../../assets/icons/NotFoundAdverts';
 
-const NotFound = () => (
+const NotFound = ({ activeEmail }) => (
   <Wrapper>
     <Container>
-      <Icon src={cat} alt="cat icon" />
-      <h1>Сторінку не знайдено</h1>
+      {activeEmail ? <NotFoundAdverts width={200} /> : <Icon src={cat} alt="cat icon" />}
+      <h1>{activeEmail ? 'Активуйте ваш профіль, лінк з активацією надіслано Вам на почту' : 'Сторінку не знайдено'}</h1>
       <GoHomeLink to="/">На головну</GoHomeLink>
     </Container>
   </Wrapper>
 );
 
+NotFound.propTypes = {
+  activeEmail: PropTypes.bool,
+};
+NotFound.defaultProps = {
+  activeEmail: false,
+};
 export default NotFound;
