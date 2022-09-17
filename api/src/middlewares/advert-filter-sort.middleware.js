e97e8f780b76b4341a3665a7a78b9e457b9260c1
Davyd Kachor <96185501+davidkachor@users.noreply.github.com>
2022-09-17 14:57:55 +0300

    add filter by city

--- api/src/middlewares/advert-filter-sort.middleware.js
+++ api/src/middlewares/advert-filter-sort.middleware.js
@@ -3,8 +3,8 @@ const errorHandler = require('../helpers/error-handler');
 
 const advertFilterSortMiddleware = async (req, res, next) => {
   try {
-    const { search, seller, minPrice, maxPrice, sort, category } = req.query;
-    req.items = await Advert.findWithFilterAndSort(search, maxPrice, minPrice, seller, sort, category);
+    const { search, seller, minPrice, maxPrice, sort, category, city } = req.query;
+    req.items = await Advert.findWithFilterAndSort(search, maxPrice, minPrice, seller, sort, category, city);
     next();
   } catch (err) {
     errorHandler(res, err);
