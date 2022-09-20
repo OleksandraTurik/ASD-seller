0ac33c74fba193596915295f46bdf8dedd3baf74
Davyd Kachor <96185501+davidkachor@users.noreply.github.com>
2022-09-20 11:21:39 +0300

    some changes for hook

--- ui/src/components/hooks/useFetchFavorites.js
+++ ui/src/components/hooks/useFetchFavorites.js
@@ -1,26 +1,29 @@
 import { useEffect, useState } from 'react';
 import advertServices from 'services/advertServices';
 
-const useFetchExactAdvert = (arrayOfId) => {
+const useFetchFavorites = (arrayOfId) => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    const controller = new AbortController();
     (async () => {
       try {
         setLoading(true);
-        const results = await Promise.all(arrayOfId.map((id) => advertServices.getAdvertById(id)));
+        const results = await Promise.all(arrayOfId.map((id) => advertServices.getAdvertById(id, controller.signal)));
         setData(results.map(el => el.data));
+        console.log(results.map(e => e.data));
         setLoading(false);
       } catch (e) {
         setError(e);
         setLoading(false);
       }
     })();
+    return () => controller.abort();
   }, [arrayOfId]);
 
   return { data, loading, error };
 };
 
-export default useFetchExactAdvert;
+export default useFetchFavorites;
